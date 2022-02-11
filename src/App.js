2cff324abb25297faabd09b0c4111f5b3c49452c
Matthew Yu <matthew@Matthews-MacBook-Pro.local>
2022-02-11 13:36:05 -0800

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import NotesList from './components/NotesList';
+
 const App = () => {
-  return <p>hello!!!</p>;
+  return <div className='container'>'
+    <p className='text-center font-italic'>Notes App</p>
+    <NotesList />
+  </div>;
 }
 
 export default App;
---
+++ src/components/NotesList.js
@@ -0,0 +1,35 @@
+import Note from './Note';
+
+const NotesList = () => {
+
+    return (
+
+        <div className="notes-list">
+            <div className='row'>
+                <div className='col'>
+                    <Note /> 
+                </div>   
+                <div className='col'>
+                    <Note /> 
+                </div>   
+                <div className='col'>
+                    <Note /> 
+                </div>     
+            </div>
+
+            <div className='row'>
+                <div className='col'>
+                    <Note /> 
+                </div>   
+                <div className='col'>
+                    <Note /> 
+                </div>   
+                <div className='col'>
+                    <Note /> 
+                </div>     
+            </div> 
+        </div>
+    )
+}
+
+export default NotesList;
