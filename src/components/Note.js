dbef171aea2a46c705de8a5c3ddbb1b76ed41d2d
Matthew Yu <matthew.yu007@gmail.com>
2022-02-12 22:57:57 -0800

    temp

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,19 @@
-const Note = () => {
+import { useState } from 'react';
 
+const Note = () => {
+    const [noteContent, setNote] = useState('');
+
+
+    const saveToState = (event) => {
+        console.log(event.target.value);
+        setNote(event.target.value);
+    }
     return (
         <div className="note">
-            <div className="card mt-4">
+            <div className="card mt-4 shadow-sm">
                 <div className="card-body">
-                    <div class="form-group">
-                        <textarea class="form-control rounded-0 border-0 " id="exampleFormControlTextarea1" rows="10" placeholder="✏️  Write something..."></textarea>
+                    <div className="form-group">
+                        <textarea className="form-control rounded-0 border-0 " id="exampleFormControlTextarea1" rows="15" placeholder="✏️  Write something..." onChange={saveToState} value={noteContent}></textarea>
                     </div>
                 </div>
             </div>
