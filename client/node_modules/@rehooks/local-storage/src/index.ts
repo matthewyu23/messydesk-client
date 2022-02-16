47b0342bf10627120f5007140ee42c7100ccbaa9
Matthew Yu <matthew.yu007@gmail.com>
2022-02-15 17:33:19 -0800

    init client

--- client/node_modules/@rehooks/local-storage/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { useLocalStorage } from './use-localstorage';
-
-export { writeStorage, deleteFromStorage } from './local-storage-events';
-
-export { useLocalStorage };
-
-export default useLocalStorage;
