5f9b71299eb1803ec4a671a43e3388c6274a968c
Matthew Yu <matthew.yu007@gmail.com>
2022-02-16 12:04:47 -0800

    connected to heroku

--- src/AppElement.js
+++ src/AppElement.js
@@ -72,7 +72,7 @@ export default function AppElement() {
   
     useEffect(() => {
       
-      const s = io('http://localhost:3001')
+      const s = io('https://messydesk.herokuapp.com/')
       setSocket(s);
       return () => {
         s.disconnect();
